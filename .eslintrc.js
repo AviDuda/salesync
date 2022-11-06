9447681aff3bc339978926648221ba05a301bfd5
Avi Duda <368677+AviDuda@users.noreply.github.com>
2022-11-06 15:38:08 +0100

    ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,13 @@ module.exports = {
     "@remix-run/eslint-config",
     "@remix-run/eslint-config/node",
     "@remix-run/eslint-config/jest-testing-library",
-    "prettier",
+    "plugin:tailwindcss/recommended",
+    "plugin:prettier/recommended",
   ],
   env: {
     "cypress/globals": true,
   },
-  plugins: ["cypress"],
+  plugins: ["cypress", "tailwindcss"],
   // We're using vitest which has a very similar API to jest
   // (so the linting plugins work nicely), but we have to
   // set the jest version explicitly.
@@ -18,4 +19,17 @@ module.exports = {
       version: 28,
     },
   },
+  rules: {
+    "prettier/prettier": ["warn", {}, { usePrettierrc: true }],
+  },
+  overrides: [
+    {
+      files: ["**/package.json"],
+      plugins: ["json-files"],
+      rules: {
+        "json-files/require-unique-dependency-names": "error",
+        "json-files/sort-package-json": "warn",
+      },
+    },
+  ],
 };
